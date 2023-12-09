cb6ff39ec4a397d3fc7904b3823da77427c6e0b6
Raventric <78981416+Ravencentric@users.noreply.github.com>
2023-12-09 21:15:18 +0530

    chore(style): ruff format

--- src/juicenet/resume.py
+++ src/juicenet/resume.py
@@ -21,6 +21,7 @@ class Resume:
 
     This class is used to manage resume information, including logging file details and checking for already uploaded files
     """
+
     def __init__(self, path: Path, scope: str, disable: bool = False) -> None:
         self.path = path
         self.scope = scope
@@ -33,7 +34,9 @@ class Resume:
         if not self.disable:
             info = get_file_info(file)
             info["scope"] = self.scope
-            csv.DictWriter(self.path.open("a"), fieldnames=["name", "size", "count", "scope"], quoting=csv.QUOTE_ALL).writerow(info)
+            csv.DictWriter(
+                self.path.open("a"), fieldnames=["name", "size", "count", "scope"], quoting=csv.QUOTE_ALL
+            ).writerow(info)
             logger.debug(f"Writing file information to resume: {file}")
 
     def filter_uploaded_files(self, files: list[Path]) -> list[Path]:
@@ -42,7 +45,11 @@ class Resume:
         """
         if not self.disable:
             not_uploaded = []
-            resume_data = tuple(csv.DictReader(self.path.open("r"), fieldnames=["name", "size", "count", "scope"], quoting=csv.QUOTE_ALL))
+            resume_data = tuple(
+                csv.DictReader(
+                    self.path.open("r"), fieldnames=["name", "size", "count", "scope"], quoting=csv.QUOTE_ALL
+                )
+            )
 
             for file in files:
                 info = get_file_info(file)
@@ -51,12 +58,12 @@ class Resume:
                     logger.info(f"Skipping: {file.name} - Already uploaded")
                 else:
                     not_uploaded.append(file)
-            
+
             return not_uploaded
-        
+
         else:
             return files
-        
+
     def clear_resume(self) -> None:
         """
         Clear resume data
