828dd1bd972dcf9a9ef5c1d3379ad2a3341994e7
Raventric <78981416+Ravencentric@users.noreply.github.com>
2024-01-01 18:16:52 +0530

    chore: remove unused dependencies

--- src/juicenet/compat.py
+++ src/juicenet/compat.py
@@ -1,18 +1,14 @@
 import sys
 
 if sys.version_info[0:2] < (3, 11):
-    import tomli as tomllib
     from strenum import StrEnum
-    from typing_extensions import NotRequired, TypedDict
 else:
     from enum import StrEnum
-    from typing import NotRequired, TypedDict
 
-    import tomllib
 
 if sys.version_info[0:2] < (3, 10):
     import importlib_metadata as metadata
 else:
     from importlib import metadata
 
-__all__ = ["metadata", "tomllib", "StrEnum", "NotRequired", "TypedDict"]
+__all__ = ["metadata", "StrEnum"]
